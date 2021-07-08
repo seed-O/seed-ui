ea088a5fb765250273b96b28653a39709960b3c5
switch-coder <sang_hk@naver.com>
2021-07-08 09:18:51 +0900

    typo 완료

--- src/typography/Typography.tsx
+++ src/typography/Typography.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { TypographyType } from "./typography.types";
 import "../../sass/main.scss";
 
-const Typograpy: React.FC<TypographyType> = ({
-  props: { color = "blackT87", size = "body1", bold = "inherit", align = "left" },
-  children,
-}) => {
+const Typograpy: React.FC<TypographyType<HTMLSpanElement>> = (
+  { props: { color = "blackT87", size = "body1", bold = "inherit", align = "left" }, children },
+  props
+) => {
   return (
-    <span data-testid={`seed-typograpy`} className={`${size} ${color}`}>
+    <span
+      data-testid={`seed-typograpy`}
+      className={`typo-${size} ${color} typo-weight${bold} typo-align${align}`}
+      {...props}
+    >
       {children}
     </span>
   );
--- src/typography/typography.types.ts
+++ src/typography/typography.types.ts
@@ -1,47 +1,31 @@
-import React, { ReactNode } from "react";
+import React from "react";
+import { color } from "../types/color.types";
 
-interface size  {
+export interface size  {
      size : "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "s1" | "s2" | "body1" | "body2" | "button" | "caption" | "overline";  
 }
 
-interface align { 
-    align :  "center" | "left"  | "right";
+export interface align { 
+    align :  "center" | "left"  | "right" | "justify";
 }
 
-interface primaryColor { 
-    primaryColor : "primary900" | "primary800" | "primary700" | "primary600" | "primary500" | "primary400" | "primary300" | "primary200" | "primary100" | "primary50" | "primaryT48" | "primaryT40" | "primaryT32" | "primaryT24" | "primaryT16" | "primaryT8";
-}
-interface blackColor {
-    black: "black900" | "black800" | "black700" | "black600" | "black500" | "black400" | "black300" | "black200" | "black100" | "black50" | "blackT48" | "blackT40" | "blackT32" | "blackT24" | "blackT16" | "blackT8";
-}
-interface generalColor {
-    general: "blackT87" | "blackT60" | "blackT38";
-}
-interface supportColor {
-    support: "success600" | "info600" | "warning600" | "error600";
-}
-
-interface bold {
+export interface bold {
     bold: "bold" | "normal" | "initial" | "inherit" | "900" | "800" | "700" | "600" | "500" | "400" | "300" | "200" | "100";
 }
 
-export interface color {color : primaryColor['primaryColor'] | blackColor['black'] | generalColor['general'] | supportColor['support']}
 
-
-export interface TypographyType<D extends React.ElementType = 'span'> {
-    props: {
+export interface TypographyType<D> extends React.DetailedHTMLProps<React.HTMLAttributes<D>,D> {
+    props?: {
       align?: align['align'];
       color?: color['color'];
       display?: 'initial' | 'block' | 'inline';
-      bold?:bold;
-      size?:size;
+      bold?:bold['bold'];
+      size?:size['size'];
     };
-    component?:D ;
+
     classKey?: size;
   }
 
-interface Typography<D extends React.ElementType = 'span'> {
-}
 /* 
  1. 크기 변경
  2. 정렬 변경
