5d4cd95acf574a34dd4a542fee9ac90e8ea4c3a1
switch-coder <sang_hk@naver.com>
2021-07-13 15:58:07 +0900

    프로젝트 이름변경

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import SeedButton from "./SeedButtons/SeedButton";
+import SeedIconButton from "./SeedButtons/SeedIconButton";
 import Typography from "./typography/Typography";
 import SeedInput from "./SeedInput/SeedInput";
+import SeedDialog, {SeedDialogAction, SeedDialogTitle} from './SeedDialog/SeedDialog';
 import "../sass/main.scss";
 
-export {  SeedInput, Typography, SeedButton };
+export {  SeedInput, Typography, SeedButton, SeedIconButton, SeedDialog, SeedDialogAction, SeedDialogTitle };
